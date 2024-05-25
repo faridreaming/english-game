a010d5a185a3e151072ac69422b780d87e8915b6
faridrawing <muhammad.farid.yamin@gmail.com>
2024-05-26 01:08:21 +0700

    fix: fix color bugs

--- dist/js/script.js
+++ dist/js/script.js
@@ -14,28 +14,38 @@ const categoryList = [
 ];
 
 let icon = "";
+let colorPrimary = "";
 
 switch (category) {
   case "grammar":
     navLinks[0].classList.add("active");
     icon = navLinks[0].querySelector("div svg");
-    primaryColor = "sky-500";
+    colorPrimary = "#06b6d44b";
+    textColor = "text-[#06b6d4]";
+    bgColor = "bg-[#06b6d4]";
+    borderColor = "border-[#06b6d4]/35";
     chapterContainer.innerHTML = generateChapter(categoryList[0]);
     break;
   case "vocabulary":
     navLinks[1].classList.add("active");
     icon = navLinks[1].querySelector("div svg");
-    primaryColor = "yellow-500";
+    colorPrimary = "#eab3084b";
+    textColor = "text-[#eab308]";
+    bgColor = "bg-[#eab308]";
+    borderColor = "border-[#eab308]/35";
     break;
   case "reading":
     navLinks[2].classList.add("active");
     icon = navLinks[2].querySelector("div svg");
-    primaryColor = "emerald-500";
+    colorPrimary = "#10b9814b";
+    textColor = "text-[#10b981]";
+    bgColor = "bg-[#10b981]";
+    borderColor = "border-[#10b981]/35";
     break;
 }
 
 const topBar = document.getElementById("top-bar");
-topBar.classList.add(`bg-${primaryColor}`);
+topBar.classList.add(bgColor);
 
 function generateChapter(category) {
   const chapters = category.chapters;
@@ -43,11 +53,15 @@ function generateChapter(category) {
   chapters.forEach((chapter, number) => {
     chapterList += `
       <div class="px-8 w-full">
-        <div class="bg-${primaryColor} bg-opacity-10 border border-${primaryColor} px-4 py-16 rounded-2xl">
-          <div class="flex justify-center">
-            <h3 class="text-center"><span class="font-bold">Chapter ${number + 1}:</span> ${chapter}</h3>
+        <a href="game.html?category=${category.title}&chapter=${chapter}" class="flex hover:bg-gray/10 transition duration-200 ease-in-out border-2 border-gray/35 border-b-4 px-4 py-12 overflow-hidden rounded-2xl relative">
+          <div class="absolute right-0 scale-[3] translate-y-5 -translate-x-6 [&>svg]:fill-white ${bgColor} p-1 rounded -rotate-3">
+            ${icon.outerHTML}
           </div>
-        </div>
+          <div class="flex flex-col items-start">
+            <h2 class="text-2xl font-bold ${textColor}">${chapter}</h2>
+            <p class="text-gray/70">Chapter ${number + 1}</p>
+          </div>
+        </a>
       </div>
     `;
   });
