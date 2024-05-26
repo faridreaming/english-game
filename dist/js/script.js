b0df53d4bd12b05319a5e2c8bbec4c774a06b14f
faridrawing <muhammad.farid.yamin@gmail.com>
2024-05-27 06:37:21 +0700

    feat: add new features

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,35 +1,24 @@
 const categories = [
   {
-    title: "Word Pairs",
-    description:
-      "Find matching pairs of words. Match each word with its pair to complete the level.",
+    title: "Translation",
+    description: "Find pairs of words with suitable translations.",
     primaryColor: "#0ea5e9",
     bgColor: "bg-[#0ea5e9]",
     icon: "dist/img/swap.svg",
   },
   {
-    title: "Sentence Shuffle",
-    description:
-      "Rearrange random sentences into grammatically correct and coherent ones. Slide each sentence piece to complete the puzzle.",
+    title: "Synonim",
+    description: "Find pairs of words with the same meaning.",
     primaryColor: "#22c55e",
     bgColor: "bg-[#22c55e]",
-    icon: "dist/img/shuffle.svg",
+    icon: "dist/img/equal.svg",
   },
   {
-    title: "Blank Fit",
-    description:
-      "Fill in the blanks in sentences with the correct words or phrases. Choose words that fit the context to complete the sentence.",
+    title: "Antonym",
+    description: "Find pairs of words with opposite meanings.",
     primaryColor: "#f59e0b",
     bgColor: "bg-[#f59e0b]",
-    icon: "dist/img/blank.svg",
-  },
-  {
-    title: "Error Spot",
-    description:
-      "Identify and correct grammar errors in given sentences. Spot and correct each error to complete the challenge.",
-    primaryColor: "#ef4444",
-    bgColor: "bg-[#ef4444]",
-    icon: "dist/img/error.svg",
+    icon: "dist/img/switch.svg",
   },
 ];
 
@@ -38,7 +27,7 @@ const categoryIndex =
 const selectedCategory = categories[categoryIndex];
 
 // Update page elements with the selected category
-document.title = `English Games | ${selectedCategory.title}`;
+document.title = `Word Matching Games | ${selectedCategory.title}`;
 document.getElementById("category-title-span").textContent =
   selectedCategory.title;
 document.getElementById("description-span").textContent =
